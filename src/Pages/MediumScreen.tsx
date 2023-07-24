ee4018859fc47e18489f190a830129a931a433e7
blessingchidima <chukwuyenumblessing8@gmail.com>
2023-07-24 04:09:26 -0700

    pushing

--- src/Pages/MediumScreen.tsx
+++ src/Pages/MediumScreen.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-// import styled from "styled-components";
 import Header from "../Components/Static/Header";
 import Hero from "./Comp/Hero";
 import PostReviews from "./Comp/PostReviews";
